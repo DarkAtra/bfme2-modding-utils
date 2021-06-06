1ad078baaf0ab2c378f049c37891b820ed2ee821
DarkAtra <darkatra@web.de>
2021-06-06 21:38:25 +0200

    #1: SkyboxReader is now accurately reading the rotation field

--- map/src/main/kotlin/de/darkatra/bfme2/map/Skybox.kt
+++ map/src/main/kotlin/de/darkatra/bfme2/map/Skybox.kt
@@ -5,7 +5,6 @@ import de.darkatra.bfme2.Vector3
 data class Skybox(
 	val position: Vector3,
 	val scale: Float,
-	val rotation: Float, // FIXME: apparently not a little endian float
-	// TODO: this is probably not a string, figure out the correct format and parse it
+	val rotation: Float,
 	val textureScheme: String
 )
