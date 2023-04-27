3fa73890840875bc53515ec272f22e8b97d887f0
Tommy Schmidt <tommy.schmidt@idealo.de>
2023-04-27 21:34:01 +0200

    chore: make KTypeExtensions internal

--- map/src/main/kotlin/de/darkatra/bfme2/map/KTypeExtensions.kt
+++ map/src/main/kotlin/de/darkatra/bfme2/map/KTypeExtensions.kt
@@ -3,7 +3,7 @@ package de.darkatra.bfme2.map
 import kotlin.reflect.KClass
 import kotlin.reflect.KType
 
-fun KType.toKClass(): KClass<*> {
+internal fun KType.toKClass(): KClass<*> {
     val classOfT = classifier
     if (classOfT !is KClass<*>) {
         error("Could not get KClass for KType '${this}'.")
